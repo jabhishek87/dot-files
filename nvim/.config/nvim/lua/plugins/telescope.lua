309c0f15fc9457ca9e84150adfd6469b0577c520
AbhishekJ <abhishekjaiswal.kol@gmail.com>
2025-11-21 11:16:53 +0000

    create new old barnch and cleaning up main

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++
@@ -1,9 +0,0 @@
-return {
-  'nvim-telescope/telescope.nvim', tag = 'v0.1.9',
-  dependencies = { 'nvim-lua/plenary.nvim' },
-  config = function()
-    local builtin = require("telescope.builtin")
-    vim.keymap.set('n', '<C-p>', builtin.find_files, {})
-    vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-  end
-}
