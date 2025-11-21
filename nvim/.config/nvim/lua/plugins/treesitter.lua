9f471aed0935837413ad222ce84d1028d15f47a6
AbhishekJ <abhishekjaiswal.kol@gmail.com>
2025-11-21 05:09:54 +0000

    nvim settings

---
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,20 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+      local config = require("nvim-treesitter.configs")
+      config.setup({
+        auto_install = true,
+        ensure_installed = {
+          "bash",
+          "lua",
+	  "vim",
+	  "python",
+        },
+        highlight = { enable = true },
+        indent = { enable = false },
+      })
+    end
+  }
+}
